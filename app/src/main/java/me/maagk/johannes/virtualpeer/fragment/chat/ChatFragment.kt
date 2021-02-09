97139ebaeaad620bfd37aa6296771ae98606f995
Johannes Maagk <jmaagk@gmail.com>
2021-02-09 17:45:57 +0100

    Add an emoji question message to the chat
    This can temporarily be shown by typing "emoji" into the chat.
    It's one of several types of messages that will be added in the future.

--- app/src/main/java/me/maagk/johannes/virtualpeer/fragment/chat/ChatFragment.kt
+++ app/src/main/java/me/maagk/johannes/virtualpeer/fragment/chat/ChatFragment.kt
@@ -12,20 +12,26 @@ import com.google.android.material.floatingactionbutton.FloatingActionButton
 import com.google.android.material.textfield.TextInputEditText
 import me.maagk.johannes.virtualpeer.R
 import me.maagk.johannes.virtualpeer.fragment.FragmentActionBarTitle
+import me.maagk.johannes.virtualpeer.survey.question.EmojiQuestion
+import me.maagk.johannes.virtualpeer.survey.question.Question
 
 class ChatFragment : Fragment(R.layout.fragment_chat), FragmentActionBarTitle {
 
     override val actionBarTitle: String
         get() = getString(R.string.nav_drawer_chat)
 
-    class Message(val type: Int, val message: String) {
+    open class Message(val type: Int, val message: String) {
 
         companion object {
             const val INCOMING = 0
             const val OUTGOING = 1
+            const val EMOJI_QUESTION = 2
         }
 
     }
+
+    abstract class QuestionMessage(type: Int, message: String, val question: Question) : Message(type, message)
+    class EmojiQuestionMessage(message: String, val emojiQuestion: EmojiQuestion) : QuestionMessage(EMOJI_QUESTION, message, emojiQuestion)
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
@@ -43,41 +49,72 @@ class ChatFragment : Fragment(R.layout.fragment_chat), FragmentActionBarTitle {
 
         val sendButton: FloatingActionButton = view.findViewById(R.id.send)
         sendButton.setOnClickListener {
-            messages.add(Message(Message.OUTGOING, inputField.text.toString()))
-            messages.add(Message(Message.INCOMING, inputField.text.toString()))
+            val userInput = inputField.text.toString()
+            messages.add(Message(Message.OUTGOING, userInput))
+
+            when(userInput) {
+                "emoji" -> {
+                    val emojis = arrayListOf("\uD83D\uDC4E", "\uD83D\uDC4D")
+                    val emojiQuestion = EmojiQuestion("", emojis)
+                    messages.add(EmojiQuestionMessage(userInput, emojiQuestion))
+                }
+
+                else -> {
+                    messages.add(Message(Message.INCOMING, inputField.text.toString()))
+                }
+            }
+
             adapter.notifyDataSetChanged()
         }
     }
 
-    private class ChatAdapter(val messages: ArrayList<Message>) : RecyclerView.Adapter<RecyclerView.ViewHolder>() {
+    private class ChatAdapter(val messages: ArrayList<Message>) : RecyclerView.Adapter<ChatAdapter.MessageViewHolder>() {
 
-        private class IncomingMessageViewHolder(val itemView: View) : RecyclerView.ViewHolder(itemView) {
+        private abstract class MessageViewHolder(val itemView: View) : RecyclerView.ViewHolder(itemView) {
 
-            private val messageText: TextView = itemView.findViewById(R.id.message)
-            private var currentMessage: Message? = null
+            protected val messageText: TextView = itemView.findViewById(R.id.message)
+            protected var currentMessage: Message? = null
 
-            fun bind(message: Message) {
+            open fun bind(message: Message) {
                 messageText.text = message.message
             }
 
         }
 
-        private class OutgoingMessageViewHolder(val itemView: View) : RecyclerView.ViewHolder(itemView) {
+        private class IncomingMessageViewHolder(itemView: View) : MessageViewHolder(itemView)
+        private class OutgoingMessageViewHolder(itemView: View) : MessageViewHolder(itemView)
+        private class EmojiQuestionMessageViewHolder(itemView: View) : MessageViewHolder(itemView) {
 
-            private val messageText: TextView = itemView.findViewById(R.id.message)
-            private var currentMessage: Message? = null
+            val emoji1: TextView = itemView.findViewById(R.id.emoji1)
+            val emoji2: TextView = itemView.findViewById(R.id.emoji2)
 
-            fun bind(message: Message) {
-                messageText.text = message.message
+            override fun bind(message: Message) {
+                super.bind(message)
+
+                val emojiQuestion = (message as EmojiQuestionMessage).emojiQuestion
+                emojiQuestion.emojis.forEachIndexed{index, emoji ->
+                    when(index) {
+                        0 -> emoji1.text = emoji
+                        1 -> emoji2.text = emoji
+                        else -> TODO("do something when more emojis are supplied")
+                    }
+                }
             }
 
         }
 
-        override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
+        override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MessageViewHolder {
+            val layoutInflater = LayoutInflater.from(parent.context)
+
             return when(viewType) {
                 Message.INCOMING -> {
                     val view = layoutInflater.inflate(R.layout.view_message_incoming, parent, false)
                     IncomingMessageViewHolder(view)
+                }
+
+                Message.EMOJI_QUESTION -> {
+                    val view = layoutInflater.inflate(R.layout.view_message_question_emoji, parent, false)
+                    EmojiQuestionMessageViewHolder(view)
                 }
 
                 else -> {
@@ -87,17 +124,9 @@ class ChatFragment : Fragment(R.layout.fragment_chat), FragmentActionBarTitle {
             }
         }
 
-        override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
+        override fun onBindViewHolder(holder: MessageViewHolder, position: Int) {
             val message = messages[position]
-            when(holder.itemViewType) {
-                Message.INCOMING -> {
-                    (holder as IncomingMessageViewHolder).bind(message)
-                }
-
-                Message.OUTGOING -> {
-                    (holder as OutgoingMessageViewHolder).bind(message)
-                }
-            }
+            holder.bind(message)
         }
 
         override fun getItemCount(): Int {
